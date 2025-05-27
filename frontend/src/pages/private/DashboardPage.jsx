fc50e4f14966c880779858535a9341cc9d7c42cc
Desta Rizky <destarizkyandika24@gmail.com>
2025-05-27 12:49:07 +0700

    update tampilan course

--- frontend/src/pages/private/DashboardPage.jsx
+++ frontend/src/pages/private/DashboardPage.jsx
@@ -1,11 +1,13 @@
 import Navbar from '../../components/private/Shared/Navbar';
 import Promotion from '../../components/private/DashboardPage/PromotionBar';
+import Recently from '../../components/private/DashboardPage/Recently';
 
 const DashboardPage = () => {
     return (
         <div>
             <Navbar />
             <Promotion />
+            <Recently />
         </div>
     )
 }
