e76587ded313ce324174bc778f0b08fa3f8f1587
Desta Rizky <destarizkyandika24@gmail.com>
2025-06-03 21:29:46 +0700

    penambahan detail konten apabila user mengklik menu bagian kursus di bagian home

--- frontend/src/routes/PrivateRoutes.jsx
+++ frontend/src/routes/PrivateRoutes.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import DashboardPage from '../pages/private/DashboardPage';
+import DetailContent from '../pages/private/DetailContent';
 import InprogressNone from '../pages/private/mycourses/Inprogressnone';
 import CompletedNone from '../pages/private/mycourses/completednone';
 import Inprogress from '../pages/private/mycourses/Inprogress';
@@ -10,6 +11,7 @@ const PrivateRoutes = () => {
     return (
         <Routes>
             <Route path="/dashboard/home" element={<DashboardPage />} />
+            <Route path="/dashboard/detailcontent" element={<DetailContent />} />
             <Route path="/dashboard/mycourses/Inprogressnone" element={<InprogressNone />} />
             <Route path="/dashboard/mycourses/Completednone" element={<CompletedNone />} />
             <Route path="/dashboard/mycourses" element={<Inprogress />} />
