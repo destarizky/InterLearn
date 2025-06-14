8629b503f51618638250592deefb6789e4397a4a
Mahdewa <brahmamahdewa03@gmail.com>
2025-06-14 08:21:11 +0700

    add leaderboard

--- backend/app.js
+++ backend/app.js
@@ -14,6 +14,7 @@ app.use('/api/auth', require('./routes/auth.routes'));
 app.use('/api/materi', require('./routes/materi.routes'));
 app.use('/api/rapot', require('./routes/rapot.routes'));
 app.use('/api/quiz', require('./routes/quiz.routes'));
+app.use('/api/leaderboard', require('./routes/leaderboard.routes'));
 
 
 const PORT = process.env.PORT || 5000;
